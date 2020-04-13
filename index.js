466a15b8e5024320b6d3b59e8ac5bee6a60ca19a
Christopher Imrie <ckimrie@gmail.com>
2020-04-13 17:08:54 +0100

    Fixes incorrect action startup

--- index.js
+++ index.js
@@ -1,32 +1,19 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
 const axios = require('axios').create({
     baseURL: 'https://5xdb6rp2d4.execute-api.eu-west-1.amazonaws.com/prod/',
 });
 
-try {
-    const token = core.getInput('token');
-    console.log(`Creating new release`);
-
-    core.setOutput('time', time);
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2);
-    console.log(`The event payload: ${payload}`);
-} catch (error) {
-    core.setFailed(error.message);
-}
-
-
 async function init() {
     console.log(`Git SHA [${process.env.GITHUB_SHA}]`);
     const token = core.getInput('token');
-    console.log(`Received token [${token}]`);
-    console.log('Creating release');
+    console.log(`Received token: ${token}`);
+    console.log('Creating release...');
     const result = await createRelease();
     console.log(`Release created: ${result.id}`);
 
-    console.log(`Checking release status`);
+    console.log(`Checking release status...`);
     const release = await getRelease(result.id);
+    console.log(`Release status retrieved:`, JSON.stringify(release));
 
     core.setOutput("releaseDecision", release.releaseStatus);
     if(release.releaseStatus === "ALLOW"){
